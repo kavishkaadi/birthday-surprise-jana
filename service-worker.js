b616fc406e8a45a558954765e942b91170b4e599
Kavishka adithya <134644885+kavishkaadi@users.noreply.github.com>
2026-08-12 16:46:47 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='birthday-surprise-v9';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon.svg','./assets/photo1.jpg','./assets/photo2.jpg','./assets/final-photo-optimized.jpg'];
+const CACHE='birthday-surprise-v10';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon.svg','./assets/photo1-optimized.jpg','./assets/photo2.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;}).catch(()=>r))));
