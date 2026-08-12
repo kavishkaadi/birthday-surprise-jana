978da58d3320c1b42d6aa49d2fec9368852314fa
Kavishka adithya <134644885+kavishkaadi@users.noreply.github.com>
2026-08-12 17:07:20 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='birthday-surprise-v11';
+const CACHE='birthday-surprise-v13';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon.svg','./assets/photo1-fast.jpg','./assets/photo2-fast.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
